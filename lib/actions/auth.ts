a79e42dfefcb367402c22ab715eb884f18f20ff3
DildosMan-star <qweryu0000@gmail.com>
2026-08-20 16:51:25 +0700

    Update auth.ts

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -22,7 +22,7 @@ export async function signUp(formData: FormData) {
   const password = String(formData.get("password") ?? "");
   const fullName = String(formData.get("full_name") ?? "");
 
-  const supabase = createClient();
+  const supabase = await createClient();
   const { error } = await supabase.auth.signUp({
     email,
     password,
@@ -38,7 +38,7 @@ export async function signUp(formData: FormData) {
 }
 
 export async function signOut() {
-  const supabase = createClient();
+  cconst supabase = await createClient();
   await supabase.auth.signOut();
   redirect("/login");
 }
